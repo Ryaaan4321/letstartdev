07086504bfecbf690f4f9743252c0c71270ab96b
Ryaaan4321 <aryan056400@gmail.com>
2024-11-18 22:11:48 +0530

    18.11.24

--- README.md
+++ README.md
@@ -2,6 +2,3 @@
 
 
 
-git remote add origin https://github.com/Ryaaan4321/letstartdev.git
-git branch -M main
-git push -u origin main
--- week6/week6.3/frontend/src/App.jsx
+++ week6/week6.3/frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [exchange1Data, setexchnage1Data] = useState({});
+  const [exchange2Data, setexchnage2Data] = useState({});
+  const [bankdata, setbankdata] = useState({});
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    setexchnage1Data({
+      return: 100,
+    });
+  }, []);
+  useEffect(() => {
+    setexchnage2Data({
+      return: 100,
+    });
+  }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setbankdata({
+        income: 100,
+      });
+    }, 5000);
+  }, []);
+  const totalReturn=exchange1Data.return+exchange2Data.return;
+  const incometax=(totalReturn+bankdata.income)*0.3;
+  return <>
+  <div>
+    hi there your income tax is {incometax}
+  </div>
+  </>
 }
 
-export default App
+export default App;
