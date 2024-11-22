53d71813f699a14a9286aba41ced0ee41ae447f1
Ryaaan4321 <aryan056400@gmail.com>
2024-11-22 23:31:19 +0530

    routing applications

--- week7/week7.1/frontend/src/App.jsx
+++ week7/week7.1/frontend/src/App.jsx
@@ -1,7 +1,51 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Dashboard from "./components/DashBoard";
+// import Landing from "./components/Landing";
+// import { useNavigate } from "react-router-dom";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       <BrowserRouter>
+//       <AppBar/>
+//         <Routes>
+//           <Route path="/dashboard" element={<Dashboard />}></Route>
+//           <Route path="/" element={<Landing />}></Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// function AppBar() {
+//   const navigate = useNavigate();
+//   return (
+//     <>
+//       <div>
+//         <button
+//           onClick={() => {
+//             navigate("/");
+//           }}
+//         >
+//           Landing Page
+//         </button>
+//         <button
+//           onClick={() => {
+//             navigate("/dashboard");
+//           }}
+//         >Dashboard Page</button>
+//       </div>
+//     </>
+//   );
+// }
+
+import React, { Suspense, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./components/DashBoard";
-import Landing from "./components/Landing";
+const Dashboard = React.lazy(() => import("./components/DashBoard")); // another way of importing the file
+const Landing = React.lazy(() => import("./components/Landing")); //another way of importing the file to get the better page performance
 import { useNavigate } from "react-router-dom";
 
 function App() {
@@ -10,10 +54,24 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      <AppBar/>
+        <AppBar />
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="/" element={<Landing />}></Route>
+          <Route
+            path="/dashboard"
+            element={
+              <Suspense fallback={"loading..."}>
+                <Dashboard />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={"loading..."}>
+                <Dashboard />
+              </Suspense>
+            }
+          ></Route>
         </Routes>
       </BrowserRouter>
     </>
@@ -36,7 +94,9 @@ function AppBar() {
           onClick={() => {
             navigate("/dashboard");
           }}
-        >Dashboard Page</button>
+        >
+          Dashboard Page
+        </button>
       </div>
     </>
   );
