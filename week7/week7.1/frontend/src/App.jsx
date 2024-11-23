fbbb81e1458b8b651de8c0bf062c39fb282bea61
Ryaaan4321 <aryan056400@gmail.com>
2024-11-23 15:56:43 +0530

    23.11.24

--- week7/week7.1/frontend/src/App.jsx
+++ week7/week7.1/frontend/src/App.jsx
@@ -4,6 +4,9 @@
 // import Landing from "./components/Landing";
 // import { useNavigate } from "react-router-dom";
 
+import { set } from "mongoose";
+import { useState } from "react";
+
 // function App() {
 //   const [count, setCount] = useState(0);
 
@@ -42,62 +45,95 @@
 //   );
 // }
 
-import React, { Suspense, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-const Dashboard = React.lazy(() => import("./components/DashBoard")); // another way of importing the file
-const Landing = React.lazy(() => import("./components/Landing")); //another way of importing the file to get the better page performance
-import { useNavigate } from "react-router-dom";
+// routing applications starts herer
 
+// import React, { Suspense, useState } from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// const Dashboard = React.lazy(() => import("./components/DashBoard")); // another way of importing the file
+// const Landing = React.lazy(() => import("./components/Landing")); //another way of importing the file to get the better page performance
+// import { useNavigate } from "react-router-dom";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <AppBar />
+//         <Routes>
+//           <Route
+//             path="/dashboard"
+//             element={
+//               <Suspense fallback={"loading..."}>
+//                 <Dashboard />
+//               </Suspense>
+//             }
+//           ></Route>
+//           <Route
+//             path="/"
+//             element={
+//               <Suspense fallback={"loading..."}>
+//                 <Dashboard />
+//               </Suspense>
+//             }
+//           ></Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// function AppBar() {
+//   const navigate = useNavigate();
+//   return (
+//     <>
+//       <div>
+//         <button
+//           onClick={() => {
+//             navigate("/");
+//           }}
+//         >
+//           Landing Page
+//         </button>
+//         <button
+//           onClick={() => {
+//             navigate("/dashboard");
+//           }}
+//         >
+//           Dashboard Page
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
+
+
+/*
+so what is prop drilling so when we have a parent and that parent has a children and that children has a another childrent
+and that children has another children(c1) in that case if the c1 need the props that only the first parent have of this chainn 
+than doesnt matter teh rest of the parent want them bt they have to pass it to them to pass it to the c1 its like we are drilling down and pushing 
+the props when we are drilling that will make our wesbite go ugly or slow  
+*/
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <BrowserRouter>
-        <AppBar />
-        <Routes>
-          <Route
-            path="/dashboard"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Dashboard />
-              </Suspense>
-            }
-          ></Route>
-          <Route
-            path="/"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Dashboard />
-              </Suspense>
-            }
-          ></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <>
+  <Count />
+  <Buttons/>
+  </>;
+}
+function Count() {
+  return 
 }
 
-function AppBar() {
-  const navigate = useNavigate();
+function Buttons() {
+  const [count,setCount]=useState(0);
   return (
     <>
-      <div>
-        <button
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Landing Page
-        </button>
-        <button
-          onClick={() => {
-            navigate("/dashboard");
-          }}
-        >
-          Dashboard Page
-        </button>
-      </div>
+      <button onClick={() => {
+        setCount(count+1);
+      }}>Increment</button>
+      <button onClick={() => {
+        setCount(count-1);
+      }}>Decrement</button>
     </>
   );
 }
