691a4ed8c7d63586f166c7d375609a5018aba1f8
Ryaaan4321 <aryan056400@gmail.com>
2024-11-27 00:44:15 +0530

    push from the 7.2

--- week7/frontend/src/App.jsx
+++ week7/frontend/src/App.jsx
@@ -1,5 +1,11 @@
-import { useRecoilState, useRecoilValue, RecoilRoot } from "recoil";
+import {
+  useRecoilState,
+  useRecoilValue,
+  RecoilRoot,
+  useSetRecoilState,
+} from "recoil";
 import { countAtom } from "./store/atoms/count";
+import { useMemo } from "react";
 
 function App() {
   return (
@@ -23,15 +29,33 @@ function Count() {
 
 function CountRender() {
   const count = useRecoilValue(countAtom);
-  return <div>{count}</div>;
+  return (
+    <div>
+      {count}
+      <EvenCountRenderer />
+    </div>
+  );
+}
+
+function EvenCountRenderer() {
+  // one way of rendering the iseven in our app another way is the selectors
+  const count = useRecoilValue(countAtom);
+  const isEven = useMemo(() => {
+    return count % 2 == 0;
+  }, [count]);
+  return <div>{isEven ? "it is even" : null}</div>;
 }
 
 function Buttons() {
-  const [count, setCount] = useRecoilState(countAtom);
+  const setcount = useSetRecoilState(countAtom);
+  // becuase if i use this will make the render the whole componenet
+  // whether in this case we only want to change the setcount to be changed so it doesnt mean to
+  // to have the count here so instead of the userecoilstate we can use
+  console.log("button re render");
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setcount((count) => count + 1)}>Increment</button>
+      <button onClick={() => setcount((count) => count - 1)}>Decrement</button>
     </>
   );
 }
