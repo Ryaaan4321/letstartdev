358ea34e8d56a82e4105b43372c70c5ac7137587
Ryaaan4321 <aryan056400@gmail.com>
2024-11-27 23:46:31 +0530

    27.11.24

--- week7/week7.3/frontend/src/App.jsx
+++ week7/week7.3/frontend/src/App.jsx
@@ -4,14 +4,14 @@ import {
   useRecoilValue,
   useSetRecoilState,
 } from "recoil";
-import { jobAtom, messaging, networkAtom, notification } from "./atom";
+import { jobAtom,  messagingAtom,  networkAtom,  notificationAtom, totalNotificationSelector } from "./atom";
+import { useMemo } from "react";
 
 function App() {
   return (
     <>
       <RecoilRoot>
         <MainApp />
-        <ButtonUpdater />
       </RecoilRoot>
     </>
   );
@@ -21,31 +21,41 @@ function MainApp() {
   console.log("main app is called");
   const networkNotification = useRecoilValue(networkAtom);
   const jobcount = useRecoilValue(jobAtom);
-  const Notificationcount = useRecoilValue(notification);
+  const Notificationcount = useRecoilValue(notificationAtom);
+  const messagecount = useRecoilValue(messagingAtom);
 
+  const totalNotification = useMemo(() => { // i can do the same thing witht the selector also so lets see 
+    // the syntax of that also
+    return jobcount + Notificationcount + networkNotification + messagecount;
+  }, [jobcount, messagecount, Notificationcount, networkNotification]);
+
+  const totalNotificationUsingSelector=useRecoilValue(totalNotificationSelector);
   return (
     <>
       <button>Home</button>
       <button>My network {networkNotification}+</button>
       <button>Jobs {jobcount}</button>
       <button>Notification {Notificationcount}</button>
+      <button>Messaging {messagecount}</button>
+      <button>Me {totalNotificationUsingSelector}</button>
     </>
   );
 }
-function ButtonUpdater() {
-  const [messagecount,setmessagecount] = useRecoilState(messaging);
-  return (
-    <div>
-      <button>Messaging {messagecount}</button>
-      <button
-        onClick={() => {
-          setmessagecount((c) => c + 1);
-        }}
-      >
-        Me
-      </button>
-    </div>
-  );
-}
+
+// function ButtonUpdater() {
+//   const [messagecount,setmessagecount] = useRecoilState(messaging);
+//   return (
+//     <div>
+//       <button>Messaging {messagecount}</button>
+//       <button
+//         onClick={() => {
+//           setmessagecount((c) => c + 1);
+//         }}
+//       >
+//         Me
+//       </button>
+//     </div>
+//   );
+// }
 
 export default App;
--- week7/week7.3/frontend/src/atom.js
+++ week7/week7.3/frontend/src/atom.js
@@ -1,17 +1,32 @@
-import {atom} from 'recoil'
-export const networkAtom= atom({
-    key:"networkAtom",
-    default:99
-})
-export const jobAtom=atom({
-    key:"jobAtom",
-    default:4
-})
-export const notification=atom({
-    key:"notificationAtom",
-    default:1
-})
-export const messaging=atom({
-    key:"messaginatom",
-    default:2
-})
+import { atom, selector } from 'recoil';
+
+export const networkAtom = atom({
+    key: "networkAtom",
+    default: 99
+});
+
+export const jobAtom = atom({
+    key: "jobAtom",
+    default: 4
+});
+
+export const notificationAtom = atom({
+    key: "notificationAtom",
+    default: 1
+});
+
+export const messagingAtom = atom({
+    key: "messagingAtom",
+    default: 2
+});
+
+export const totalNotificationSelector = selector({
+    key: "totalNotificationSelector",
+    get: ({ get }) => {
+        const networkCount = get(networkAtom);
+        const jobCount = get(jobAtom);
+        const notificationCount = get(notificationAtom);
+        const messageCount = get(messagingAtom);
+        return networkCount + jobCount + notificationCount + messageCount;
+    }
+});
