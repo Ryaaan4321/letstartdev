5ff1b7ee9863570992d00c4db202743b142cf266
Ryaaan4321 <aryan056400@gmail.com>
2024-12-04 08:54:06 +0530

    recoil learning

--- week7/week7.3/frontend/src/App.jsx
+++ week7/week7.3/frontend/src/App.jsx
@@ -4,43 +4,48 @@ import {
   useRecoilValue,
   useSetRecoilState,
 } from "recoil";
-import { jobAtom,  messagingAtom,  networkAtom,  notificationAtom, totalNotificationSelector } from "./atom";
+import {
+  charCountState,
+  notifications,
+  textState,
+  totalNotificationSelector,
+} from "./atom";
 import { useMemo } from "react";
 
 function App() {
   return (
     <>
       <RecoilRoot>
-        <MainApp />
+        <SelfLearning />
       </RecoilRoot>
     </>
   );
 }
 
-function MainApp() {
-  console.log("main app is called");
-  const networkNotification = useRecoilValue(networkAtom);
-  const jobcount = useRecoilValue(jobAtom);
-  const Notificationcount = useRecoilValue(notificationAtom);
-  const messagecount = useRecoilValue(messagingAtom);
+// function MainApp() {
+//   console.log("main app is called");
+//   const networkNotification = useRecoilValue(networkAtom);
+//   const jobcount = useRecoilValue(jobAtom);
+//   const Notificationcount = useRecoilValue(notificationAtom);
+//   const messagecount = useRecoilValue(messagingAtom);
 
-  const totalNotification = useMemo(() => { // i can do the same thing witht the selector also so lets see 
-    // the syntax of that also
-    return jobcount + Notificationcount + networkNotification + messagecount;
-  }, [jobcount, messagecount, Notificationcount, networkNotification]);
+//   const totalNotification = useMemo(() => { // i can do the same thing witht the selector also so lets see
+//     // the syntax of that also
+//     return jobcount + Notificationcount + networkNotification + messagecount;
+//   }, [jobcount, messagecount, Notificationcount, networkNotification]);
 
-  const totalNotificationUsingSelector=useRecoilValue(totalNotificationSelector);// using the selector check the atom.jsx
-  return (
-    <>
-      <button>Home</button>
-      <button>My network {networkNotification}+</button>
-      <button>Jobs {jobcount}</button>
-      <button>Notification {Notificationcount}</button>
-      <button>Messaging {messagecount}</button>
-      <button>Me {totalNotificationUsingSelector}</button>
-    </>
-  );
-}
+//   const totalNotificationUsingSelector=useRecoilValue(totalNotificationSelector);// using the selector check the atom.jsx
+//   return (
+//     <>
+//       <button>Home</button>
+//       <button>My network {networkNotification}+</button>
+//       <button>Jobs {jobcount}</button>
+//       <button>Notification {Notificationcount}</button>
+//       <button>Messaging {messagecount}</button>
+//       <button>Me {totalNotificationUsingSelector}</button>
+//     </>
+//   );
+// }
 
 // function ButtonUpdater() {
 //   const [messagecount,setmessagecount] = useRecoilState(messaging);
@@ -57,5 +62,38 @@ function MainApp() {
 //     </div>
 //   );
 // }
+
+function SelfLearning() {
+  return (
+    <>
+      Hello from the self Learning
+      <TextInput />
+      <CharacterCount />
+    </>
+  );
+}
+
+
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+function CharacterCount() {
+  const count = useRecoilValue(charCountState); // Get the computed character count
+
+  return <p>Character Count: {count}</p>;
+}
 
 export default App;
--- week7/week7.3/frontend/src/atom.js
+++ week7/week7.3/frontend/src/atom.js
@@ -1,12 +1,12 @@
 import { atom, selector } from 'recoil';
 
 
-export const notifications=atom({
-    key:"notificationSelector",
-    default:selector({
-        key:"networkAtomSelector",
-        get:async()=>{
-            const res=await axios.get('http//somebackendrequest.com'); // so thats how i can get rid of fetching the data on the main page 
+export const notifications = atom({
+    key: "notificationSelector",
+    default: selector({
+        key: "networkAtomSelector",
+        get: async () => {
+            const res = await axios.get('http//somebackendrequest.com'); // so thats how i can get rid of fetching the data on the main page 
             // becuase when we are fetching the data in the main page it makes the whole page rerender that can increase our website load time or render time
             // so instead of that we are here fetching the data and providing it to the places where it is needed 
             // and that will help use to decrease the number of rerender when every time application mounts
@@ -24,3 +24,15 @@ export const totalNotificationSelector = selector({
         return networkCount + jobCount + notificationCount + messageCount;
     }
 });
+export const textState = atom({
+    key: 'textState', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+  });
+  // Selector to compute the character count
+  export const charCountState = selector({
+    key: 'charCountState', // Unique key for the selector
+    get: ({ get }) => {
+      const text = get(textState); // Get the current value of textState
+      return text.length; // Return the character count
+    },
+  });
