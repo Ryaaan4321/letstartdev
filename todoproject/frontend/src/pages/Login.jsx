9ee4709385039808f506260a65c0de239efb9427
Ryaaan4321 <aryan056400@gmail.com>
2024-12-05 23:19:13 +0530

    made my first login page with the pure css that i wrote it by myself

--- todoproject/frontend/src/pages/Login.jsx
+++ todoproject/frontend/src/pages/Login.jsx
@@ -1,37 +1,31 @@
 function Login() {
   return (
     <>
-      <div>
-        <p className="text-black-800 text-3xl flex align-items ml-72">
-          {" "}
-          hmlo hmlo
-        </p>
-
-        <center>
-          <div className="flex flex-col w-56 content-center my-10">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="border-black text-black "
-            ></input>
-            <br />
-            <input
-              type="password"
-              placeholder="Enter your password"
-              className="border-black text-black"
-            ></input>
-            <br></br>
-            Sign in As:
-            <div className="flex flex-row space-x-4">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                Admin
-              </button>
-              <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-                User
-              </button>
-            </div>
-          </div>
-        </center>
+      <div className="bg-slate-100 grid justify-items-center ">
+        <p className="text-black-800 text-4xl  "> Login</p>
+        <div className="flex-col mt-4 space-y-3 ">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="border-black text-black py-2 px-4 rounded-2xl"
+          ></input>
+          <br />
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="border-black text-black  black py-2 px-4 rounded-2xl"
+          ></input>
+          <br></br>
+          Sign in As:
+        </div>
+        <div className="flex justify-between space-x-2">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            Admin
+          </button>
+          <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+            User
+          </button>
+        </div>
       </div>
     </>
   );
