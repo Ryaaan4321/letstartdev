8a4b13794c8152c61e0bb27c4fc35fc26be11d9b
Ryaaan4321 <aryan056400@gmail.com>
2024-12-05 08:47:20 +0530

    created the small assignment

--- week8/week8.1/frontend/src/App.jsx
+++ week8/week8.1/frontend/src/App.jsx
@@ -1,14 +1,33 @@
 function App() {
   return (
     <>
-     <div className="flex justify-around">
-      <div style={{color:'royalblue'}}>hmlo hmlo</div>
-      <div style={{color:'saddlebrown' ,}}>Hmlo hmlo</div>
-      <div style={{color:'red'}}>hmlo</div>
-
-     </div>
+      {/* <div className="flex justify-around">
+        <div style={{ color: "royalblue" }}>hmlo hmlo</div>
+        <div style={{ color: "saddlebrown" }}>Hmlo hmlo</div>
+        <div style={{ color: "red" }}>hmlo</div>
+      </div> */}
+      <div className="grid-cols-1 md:grid md:grid-cols-3 ">
+        <div
+          style={{ color: "royalblue" }}
+          className=" md:col-span-1 bg-slate-500"
+        >
+          hmlo hmlo
+        </div>
+        <div
+          style={{ color: "saddlebrown" }}
+          className=" md:col-span-1 bg-zinc-300"
+        >
+          Hmlo hmlo
+        </div>
+        <div
+          style={{ color: "red" }}
+          className=" md:col-span-1 bg-red-950"
+        >
+          hmlo hmlo
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
