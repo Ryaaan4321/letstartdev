35e7de56a1dd97d843a8d58e214174ead9ff3eb5
Ryaaan4321 <aryan056400@gmail.com>
2024-12-05 07:58:18 +0530

    flex classes

---
+++ week8/week8.1/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+     <div className="flex justify-around">
+      <div style={{color:'royalblue'}}>hmlo hmlo</div>
+      <div style={{color:'saddlebrown' ,}}>Hmlo hmlo</div>
+      <div style={{color:'red'}}>hmlo</div>
+
+     </div>
+    </>
+  )
+}
+
+export default App
