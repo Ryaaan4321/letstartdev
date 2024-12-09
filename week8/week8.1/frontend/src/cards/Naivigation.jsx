abeb16bb9716bdef3b5f3b743f562c37e74e1729
Ryaaan4321 <aryan056400@gmail.com>
2024-12-09 23:03:30 +0530

    completed the navigation page

--- week8/week8.1/frontend/src/cards/Naivigation.jsx
+++ week8/week8.1/frontend/src/cards/Naivigation.jsx
@@ -1,16 +1,50 @@
 function Navigation() {
   return (
     <>
-      <div className="bg-white shadow-xl flex justify-between">
-        <div className="pt-1 text-black text-3xl font-semibold">
-          <h2>Payout</h2>
-        </div>
-        <div className="pt-1">
-          <input className="px-24 py-1.5 rounded" type="text" placeholder="Search features tutorials etc" />
-        </div>
-        <div className="flex justify-between space-x-2 pt-1">
-          <div>first logo</div>
-          <div>second logo</div>
+      <div>
+        <div className="bg-white shadow-xl flex justify-between p-3">
+          <div className="pt-1 text-black text-3xl font-semibold">
+            <h2>Payout</h2>
+          </div>
+          <div className="pt-1">
+            <input
+              className="px-24 py-1.5 rounded"
+              type="text"
+              placeholder="Search features tutorials etc"
+            />
+          </div>
+          <div className="flex justify-between space-x-2 pt-2 ">
+            <div className="bg-gray-200 rounded-full p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                />
+              </svg>
+            </div>
+            <div className="bg-gray-200 rounded-full p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
     </>
