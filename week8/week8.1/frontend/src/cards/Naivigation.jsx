fd31eec957ab605f3af79d598f021ede6ad76c29
Ryaaan4321 <aryan056400@gmail.com>
2024-12-10 09:02:57 +0530

    naviagtion completed

--- week8/week8.1/frontend/src/cards/Naivigation.jsx
+++ week8/week8.1/frontend/src/cards/Naivigation.jsx
@@ -7,11 +7,40 @@ function Navigation() {
             <h2>Payout</h2>
           </div>
           <div className="pt-1">
-            <input
-              className="px-24 py-1.5 rounded"
-              type="text"
-              placeholder="Search features tutorials etc"
-            />
+            <form class="max-w-md mx-auto">
+              <label
+                for="default-search"
+                class="mb-1 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              >
+                Search
+              </label>
+              <div class="relative ">
+                <input
+                  type="search"
+                  id="default-search"
+                  class="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg !bg-white focus:ring-blue-500 focus:border-blue-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="Search Tutorial etc"
+                  required
+                />
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <svg
+                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </form>
           </div>
           <div className="flex justify-between space-x-2 pt-2 ">
             <div className="bg-gray-200 rounded-full p-2">
