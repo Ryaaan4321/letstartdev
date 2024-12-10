f6f419682a7aface94377227f06411d0d66083cd
Ryaaan4321 <aryan056400@gmail.com>
2024-12-10 08:35:27 +0530

    oen more div completed

--- week8/week8.1/frontend/src/cards/Transaction.jsx
+++ week8/week8.1/frontend/src/cards/Transaction.jsx
@@ -2,7 +2,7 @@ function Transaction() {
   return (
     <>
       <div className="grid grid-cols-2">
-        <div className="col-span-2 pt-6">
+        <div className="col-span-2 pt-6 ">
           <div className="flex  space-x-1 text-1xl font-semibold">
             <h4>Transaction |</h4>
             <h4>This Month</h4>
@@ -16,15 +16,48 @@ function Transaction() {
             Refends(6)
           </button>
         </div>
-        <div className="col-span-2 pt-6">
-          <div className="flex justify-between ">
+        <div className="col-span-2 pt-6 ">
+          <div className="flex justify-between">
             <div>
-              <input type="text" placeholder="Order Id or Transaction Id" />
+              <form class="max-w-md mx-auto">
+                <label
+                  for="default-search"
+                  class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                >
+                  Search
+                </label>
+                <div class="relative">
+                  <input
+                    type="search"
+                    id="default-search"
+                    class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg !bg-white focus:ring-blue-500 focus:border-blue-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="Search Mockups, Logos..."
+                    required
+                  />
+                  <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg
+                      class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </form>
             </div>
-            <div>
-              <div className="flex space-x-6 p-2 bg-gray-300 rounded-md shadow-sm">
-                <div className="text-gray-600">Sort</div>
-                <div>
+            <div className="flex justify-between space-x-3">
+              <div className="flex space-x-2 p-4 bg-gray-300 rounded-md shadow-sm">
+                <div className="text-gray-600 flex-1 mx-1">Sort</div>
+                <div className="flex-1 mx-2 ">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
