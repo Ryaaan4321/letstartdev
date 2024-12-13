d141804f4517b23d3ded9bec2732f232528fc7aa
Ryaaan4321 <aryan056400@gmail.com>
2024-12-13 08:27:04 +0530

    server is listening on the port 6996

--- week8/week8.2/backend/index.js
+++ week8/week8.2/backend/index.js
@@ -1,5 +1,10 @@
 import express from 'express'
-
+import router from './routes/mainrouter.js'
 const app=express();
 
-app.use('/api/v1',mainrouter);
+app.use(express.json());
+app.use('/api/v1',router);
+
+app.listen(3000,()=>{
+    console.log(`server is listening in the port ${3000}`);
+})
