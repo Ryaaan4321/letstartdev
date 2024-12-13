5fc74f58ad16defdd4eaec13cf486f811aa57504
Ryaaan4321 <aryan056400@gmail.com>
2024-12-13 08:42:03 +0530

    added routes

--- week8/week8.2/backend/index.js
+++ week8/week8.2/backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import router from './routes/mainrouter.js'
+import authrouter from './routes/auth.router.js'
+import userrouter from './routes/user.router.js'
 const app=express();
 
 app.use(express.json());
-app.use('/api/v1',router);
+app.use('/api/v1',authrouter);
+app.use('/api/v2',userrouter);
 
 app.listen(3000,()=>{
     console.log(`server is listening in the port ${3000}`);
