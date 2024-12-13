93e519268ee34c508ea457b29bff23ae641edf60
Ryaaan4321 <aryan056400@gmail.com>
2024-12-13 08:56:02 +0530

    changes

--- week8/week8.2/backend/routes/user.router.js
+++ week8/week8.2/backend/routes/user.router.js
@@ -1,5 +1,18 @@
 import expres from 'express';
+import zod from 'zod'
 
 const router=expres.Router();
+
+const signupSchema=zod.object({
+    firstname:zod.string(),
+    secondname:zod.string(),
+    username:zod.string(),
+    password:zod.string()
+})
+
+router.post('/signup',async(req,res)=>{
+  const {firstname,secondname,username,password}=req.body;
+
+});
 
 export default router;
