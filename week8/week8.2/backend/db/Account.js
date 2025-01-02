4db2dd252867f833af7300aa4d492952df9e4c94
Ryaaan4321 <aryan056400@gmail.com>
2025-01-02 08:16:34 +0530

    update account schema to include user reference and balance field

--- week8/week8.2/backend/db/Account.js
+++ week8/week8.2/backend/db/Account.js
@@ -2,7 +2,14 @@ import mongoose from "mongoose";
 
 const accountSchema=new mongoose.Schema({
     userId:{
-        type:mongoose.Schema.Types.ObjectId;
-        ref
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    balance:{
+        type:Number,
+        required:true,
     }
-})
+});
+
+export const Account=mongoose.model('Account',accountSchema);
