2f1556fefff7c840879d8fa0f4e83c43111f1e7d
Ryaaan4321 <aryan056400@gmail.com>
2025-01-02 08:54:52 +0530

    add balance retrieval and transfer functionality to account router

--- week8/week8.2/backend/routes/account.router.js
+++ week8/week8.2/backend/routes/account.router.js
@@ -2,8 +2,44 @@ import express from 'express';
 import {Account} from '../db/Account.js';
 import {User} from '../db/User.js';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
 const router=express.Router();
 
+router.get('/balance',authMiddleware,async(req,res)=>{
+    const account=await Account.findOne({userId:req.user._id});
+    res.json({
+        balance:account.balance
+    })
+})
+router.post('/transfer',authMiddleware,async(req,res)=>{
+    const session=await mongoose.startSession();
+    session.startTransaction();
+    const {amount,to}=req.body;
+
+    const account=await Account.findOne({userId:req.user._id}).session(session);
+    if(!account|| account.balance<amount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            msg:"insufficeint balance"
+        })
+    }
+    const toAccount=await Account.findOne({userId:to}).session(session);
+    if(!toAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            msg:"inavalid account"
+        })
+    };
+    await Account.updateOne({userId:req.user._id},{$inc:{balance:-amount}}).session(session);
+    await Account.updateOne({userId:to},{$inc:{balance:amount}}).session(session);
+
+    await session.commitTransaction();
+
+    res.json({
+        msg:"transfered successfully"
+    });
+
+})
 
 export default router;
