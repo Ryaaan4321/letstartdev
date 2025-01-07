5d175be7e3f5fe5a5373d42e9020f4bb688475d1
Ryaaan4321 <aryan056400@gmail.com>
2025-01-07 09:08:16 +0530

    implement axios POST request to send user data and update GET request to fetch users

--- week8/week8.3/backend/index.js
+++ week8/week8.3/backend/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { response } from "express";
 function main() {
     fetch('https://jsonplaceholder.typicode.com/posts').then(async (response) => {
         const json = await response.json();
@@ -14,9 +15,23 @@ async function maina() {
 // maina();
 
 function main1(){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get('https://jsonplaceholder.typicode.com/users')
     .then((response)=>{
         console.log(response.data);
     })
 }
-main1();
+// main1();
+
+
+function main2(){
+    axios.post('https://httpdump.app/dumps/e96da8fa-e0ca-4b9b-a7f9-8d1229a2a8eb',
+    {
+        username:"aryan",
+        email:"aa@gmail.com",
+    },{
+        headers:"Bearer 123"
+    }).then((response)=>{
+        console.log(response);
+    })
+}
+main2();
