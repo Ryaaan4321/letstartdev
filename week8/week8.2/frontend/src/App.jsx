70bb404f76a62ce28f8493573040a180068c86d7
Ryaaan4321 <aryan056400@gmail.com>
2025-01-08 09:14:40 +0530

    implement routing for signup page and create Signup component with UI structure

--- week8/week8.2/frontend/src/App.jsx
+++ week8/week8.2/frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import { Signup } from './pages/Signup'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      hhghh
+    <BrowserRouter>
+    <Routes>
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+    </BrowserRouter>
+      
     </>
   )
 }
---
+++ week8/week8.2/frontend/src/pages/Signup.jsx
@@ -0,0 +1,17 @@
+import { BottomWarning } from "../components/BottomWarning";
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/InputBox";
+import { SubHeading } from "../components/SubHeading";
+import { Button } from "../components/Button";
+
+
+export function Signup(){
+    return <div className="bg-slate-300 h-screen flex justify-center">
+        <div className="flex flex-col justify-center">
+            <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+                <Heading label={"Signup"}/>
+            </div>
+        </div>
+
+    </div>
+}
