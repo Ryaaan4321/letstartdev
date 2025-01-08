a80eb94e811c7b2064f151fd8de6bda9706444a9
Ryaaan4321 <aryan056400@gmail.com>
2025-01-08 22:35:33 +0530

    add Signup component with input fields and buttons for user registration

--- week8/week8.2/frontend/src/pages/Signup.jsx
+++ week8/week8.2/frontend/src/pages/Signup.jsx
@@ -10,6 +10,13 @@ export function Signup(){
         <div className="flex flex-col justify-center">
             <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                 <Heading label={"Signup"}/>
+                <SubHeading label={"Enter your credentials"}/>
+                <InputBox label={"FirstName"}/>
+                <InputBox label={"SecondName"}/>
+                <InputBox label={"Email"}/>
+                <InputBox label={"Password"}/>
+                <Button label={"Sign Up"} to={'somepostfunction'}/>
+                <BottomWarning label={"Already Have An Account"} buttonText={"SignIn"} to={'/signin'}/>
             </div>
         </div>
 
