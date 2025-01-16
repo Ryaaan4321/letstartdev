41d641e13ab103c7eada709131d893e800c09aa5
Ryaaan4321 <aryan056400@gmail.com>
2025-01-16 16:34:24 +0530

    feat: redesign Login page with new components; add Heading, SubHeading, and SocialMediaLoginButton for improved user experience

--- todoproject/frontend/src/pages/Login.jsx
+++ todoproject/frontend/src/pages/Login.jsx
@@ -1,37 +1,46 @@
 import { Link } from "react-router-dom";
+import { Heading } from "../components/Heading";
+import { SubHeading } from "../components/SubHeading";
+import { SocialMediaLoginButton } from "../components/SocialMediaLoginButton";
+import { Button } from "../components/Button";
+
 function Login() {
   return (
     <>
-      <div className="bg-slate-100 grid justify-items-center">
-        <p className="text-black-800 text-4xl  "> Login</p>
-        <div className="flex-col mt-4 space-y-3 ">
-          <input
-            type="email"
-            placeholder="Enter your email"
-            className="border-black text-black py-2 px-4 rounded-2xl"
-          ></input>
-          <br />
-          <input
-            type="password"
-            placeholder="Enter your password"
-            className="border-black text-black  black py-2 px-4 rounded-2xl"
-          ></input>
-          <br></br>
-        </div>
-        <div className="flex justify-between space-x-2 mt-3">
-          <h1 className="text-2xl">Sign Up As:</h1>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-            Admin
-          </button>
-          <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-            User
-          </button>
-        </div>
-        <div className="flex justify-around space-x-3 text-1xl">
-          <h3>Dont have an Account ?</h3>
-          <Link to="/signup">
-            <span className="text-blue-900">SignUp</span>
-          </Link>
+      <div className=" flex flex-col items-center ">
+        <div className="shadow-md m-4 p-5 mt-12 ">
+          <Heading label={"Welcome Again Anon..!"} />
+          <SocialMediaLoginButton />
+          <SubHeading label={"Let's explore the life again.."} />
+          <div className="flex flex-col mt-4 space-y-3 w-96 ">
+           
+            <input
+              type="text"
+              placeholder="Username"
+              className="border-black text-black  py-2 px-4 rounded-2xl"
+            ></input>
+            <input
+              type="email"
+              placeholder="Email"
+              className="border-black text-black py-2 px-4 rounded-2xl"
+            ></input>
+            <input
+              type="password"
+              placeholder="Password"
+              className="border-black text-black  py-2 px-4 rounded-2xl"
+            ></input>
+          </div>
+          <Button label={"Start Your Journey..."} />
+          <div className="flex space-x-1 mt-2">
+            <div>
+              <h3 className="text-1xl">Don't have an Account ?</h3>
+            </div>
+            <div>
+              <Link to="/signup">
+                <span className="text-blue-900">Signup</span>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
