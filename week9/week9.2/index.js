1a568183f160226d932f4dbab1ecc489a23c630c
Ryaaan4321 <aryan056400@gmail.com>
2025-01-23 08:48:25 +0530

    feat: add new greeting and summation functions with updated type handling

--- week9/week9.2/index.js
+++ week9/week9.2/index.js
@@ -2,4 +2,15 @@
 function greet(name) {
     console.log(`hello ${name}`);
 }
-greet("aryan");
+greet(86868686868);
+function greet1(name) {
+    console.log(`hmllo hmlooo ${name}`);
+}
+// greet1("aryannnnnn");
+function sum1(a, b) {
+    return a + b;
+}
+function sum2(a, b) {
+    return a + b;
+}
+console.log(sum1(1, 2) + sum2(1, 2));
--- week9/week9.2/index.ts
+++ week9/week9.2/index.ts
@@ -1,5 +1,15 @@
-function greet(name:string){
+function greet(name:any){
     console.log(`hello ${name}`);
 }
-greet("aryan");
-
+greet(86868686868);
+function greet1(name:string){
+    console.log(`hmllo hmlooo ${name}`);
+}
+// greet1("aryannnnnn");
+function sum1(a:any,b:any):any{
+    return a+b;
+}
+function sum2(a:number,b:number):any{
+    return a+b;
+}
+console.log(sum1(1,2)+sum2(1,2));
