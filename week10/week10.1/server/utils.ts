99459148f0575fc062ca25e6fc298eb601412b78
Ryaaan4321 <aryan056400@gmail.com>
2025-01-27 21:20:37 +0530

    feat: add PostgreSQL client utility and update dependencies

---
+++ week10/week10.1/server/utils.ts
@@ -0,0 +1,7 @@
+import {Client} from 'pg';
+
+export async function getClient(){
+    const client=new Client("postgresql://neondb_owner:npg_IYyg7mAp0NoP@ep-old-tooth-a8dt8acn-pooler.eastus2.azure.neon.tech/neondb?sslmode=require")
+    await client.connect();
+    return client;
+} 
