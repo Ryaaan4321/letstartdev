5e874771f0402514507690b62a9e1ad250494178
Ryaaan4321 <aryan056400@gmail.com>
2025-02-01 22:16:21 +0530

    feat: enhance fetch handler to include sum function and improve GET request handling

--- week11/week11.1/server/src/index.ts
+++ week11/week11.1/server/src/index.ts
@@ -1,15 +1,19 @@
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		console.log(request.body);
-		if(request.method=="GET"){
+		if (request.method == "GET") {
+			sum(18, "ko birthday hai bhadwe ka").then(result => console.log(result));
+			console.log(typeof(sum));
 			return Response.json({
-				msg:"this is get request"
-			})
-		}else{
+				msg: "this is get request"
+			});
+		} else {
 			return Response.json({
-				msg:"there is not a get request"
-			})
+				msg: "this is not a get request"
+			});
 		}
-	
+		async function sum(a: number, b: any) {
+			return a + b;
+		}
 	},
 } satisfies ExportedHandler<Env>;
