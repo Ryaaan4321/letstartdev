745f04171d52f1ca2b1c6c9c8bf5830385494a68
Ryaaan4321 <aryan056400@gmail.com>
2025-02-01 22:04:40 +0530

    feat: update fetch handler to differentiate GET requests and log request body

--- week11/week11.1/server/src/index.ts
+++ week11/week11.1/server/src/index.ts
@@ -1,18 +1,15 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.json`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return Response.json({msg:"hmlo hmlo there bitchh"})
+		console.log(request.body);
+		if(request.method=="GET"){
+			return Response.json({
+				msg:"this is get request"
+			})
+		}else{
+			return Response.json({
+				msg:"there is not a get request"
+			})
+		}
+	
 	},
 } satisfies ExportedHandler<Env>;
