ce6311e2ef66be3f046c3264cbe13a6815b9e09d
Ryaaan4321 <aryan056400@gmail.com>
2025-02-02 11:16:44 +0530

    feat: add GET handlers for fetching todos and a specific todo item

--- week11/week11.1/snewserver/src/index.ts
+++ week11/week11.1/snewserver/src/index.ts
@@ -9,5 +9,19 @@ app.post('/',async (c) => {
   console.log(c.req.query("param"));
   return c.text("Hmlo Hmlo");
 })
+// 
+app.get('/todos', async (c) => {
+  const url = 'https://dummyjson.com/todos';
+  const response = await fetch(url);
+  const data: any = await response.json();
+  return c.json(data);
+});
+app.get('/todos/1',async(c)=>{
+  const url='https://dummyjson.com/todos/1';
+  const response=await fetch(url);
+  console.log("response type = ",typeof(response) )
+  const data:any=await response.json();
+  return c.json(data);
+})
 
 export default app
