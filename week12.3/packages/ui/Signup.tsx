473768825c07526fecda5e0b09106f9508410dbe
Ryaaan4321 <aryan056400@gmail.com>
2025-02-05 17:01:26 +0530

    feat: simplify Home component and add Signup component

---
+++ week12.3/packages/ui/Signup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Signup() {
+    return (
+        <div style={{ width: "100px" }}>
+            <input placeholder="email" type="text"></input>
+            <input placeholder="password" type="password"></input>
+        </div>
+    );
+}
