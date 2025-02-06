47d230d236fa2c45fbd1e30a96004f180c719e5b
Ryaaan4321 <aryan056400@gmail.com>
2025-02-06 09:09:50 +0530

    feat(week12.4): initialize project with PostgreSQL setup and create users table

---
+++ week12.4/src/index.ts
@@ -0,0 +1,17 @@
+import { Client } from "pg";
+
+const client = new Client('postgresql://neondb_owner:npg_IYyg7mAp0NoP@ep-old-tooth-a8dt8acn-pooler.eastus2.azure.neon.tech/neondb?sslmode=require');
+
+async function createTable() {
+    await client.connect();
+    const result = await client.query(`
+        CREATE TABLE userss(
+        id serial PRIMARY KEY,
+        username varchar(50) UNIQUE NOT NULL,
+        email varchar(50) UNIQUE NOT NULL,
+        password varchar(250) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        )`)
+    console.log("resutl is =" , result);    
+}
+createTable();
