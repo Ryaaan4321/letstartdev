ae317d9bdecb7ffa6c12815b9941a01142ee0abd
Ryaaan4321 <aryan056400@gmail.com>
2025-02-07 08:47:02 +0530

    started the day with the dev

--- week12.4/src/index.ts
+++ week12.4/src/index.ts
@@ -4,31 +4,36 @@ const client = new Client('postgresql://neondb_owner:npg_IYyg7mAp0NoP@ep-old-too
 
 async function createTable() {
     await client.connect();
-    const result = await client.query(`
-        CREATE TABLE userss(
+    const result2 = await client.query(`CREATE TABLE ADDRESS (
         id serial PRIMARY KEY,
-        username varchar(50) UNIQUE NOT NULL,
-        email varchar(50) UNIQUE NOT NULL,
-        password varchar(250) NOT NULL,
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        user_id INTEGER NOT NULL,
+        city VARCHAR(100) NOT NULL,
+        country VARCHAR(200) NOT NULL,
+        street VARCHAR(200) NOT NULL,
+        pincode VARCHAR(20),
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
         )`)
-    console.log("resutl is =" , result);    
+    console.log("result is =", result2);
 }
 createTable();
 
-async function insertUserData(username: string, password: string, email: string) {
-    await client.connect();
-    const result = await client.query(`INSERT INTO userss (username,password,email)
-        VALUES('${username}' , '${password}' ,'${email}')`,)
+// async function insertUserData(username: string, password: string, email: string) {
+//     await client.connect();
+//     const result = await client.query(`INSERT INTO userss (username,password,email)
+//         VALUES('${username}' , '${password}' ,'${email}')`,)
+//     console.log("resutl is =", result);
+//     const result1 = await client.query(`INSERT INTO userss(username,password,email)
+//         VALUES($1,$2,$3)`, [username, password, email]);
+//     console.log(result1);
+// }
+async function insertAdressData(city:string,country:string,street:string,pincode:string,){
 
-    console.log("resutl is =", result);
 }
+// insertUserData('aryan', 'arrr@gmail.com', 'aaaaaaaa');
 
-insertUserData('aryan' ,'arrr@gmail.com' ,'aaaaaaaa');
-
-async function getAllUsers(){
-    await client.connect();
-    const result=await client.query(`select * from userss`);
-    console.log(result);
-}
-getAllUsers();
+// async function getAllUsers() {
+//     await client.connect();
+//     const result = await client.query(`select * from userss`);
+//     console.log(result);
+// }
+// getAllUsers();
