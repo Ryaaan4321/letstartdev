aa620506a84f74cf25cd8c6002acd3434baace3d
Ryaaan4321 <aryan056400@gmail.com>
2025-02-09 10:33:39 +0530

    not working

--- week12/week12.5/src/index.ts
+++ week12/week12.5/src/index.ts
@@ -20,30 +20,22 @@ async function insertUser(username: string, password: string, firstname: string,
         await prisma.$disconnect();
     }
 }
-async function getTodoAndUserDetails(userId: number) {
-    const newTodo = await prisma.todo.create({
-        data: {
-            title: "something",
-            descreption: "somehting",
-            completed: false,
-            user_id: 1
+async function getTodoAndUserDetails(userid: number) {
+    const response = await prisma.todo.findMany({
+        where: {
+            user_id: userid
         },
-    });
+        select: {
+            user_id: true,
+            title: true,
+            descreption: true,
+            user: true
+        }
+    })
+    console.log(response);
 
-    console.log('New Todo created:', newTodo);
 }
 getTodoAndUserDetails(1);
 
-// const response=await prisma.todo.findMany({
-//     where:{
-//         user_id:userid
-//     },
-//     select:{
-//         user_id:true,
-//         title:true,
-//         descreption:true,
-//         user:true
-//     }
-// })
-// console.log(response);
+
 // insertUser("ar","arr","arr","arrrr","arr@gmail.com");
