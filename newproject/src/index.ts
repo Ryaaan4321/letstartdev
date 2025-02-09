1261e99f1f9bb4a41199709aa9a167884688553d
Ryaaan4321 <aryan056400@gmail.com>
2025-02-09 23:35:09 +0530

    added to get all todos of the user

--- newproject/src/index.ts
+++ newproject/src/index.ts
@@ -1,27 +1,36 @@
-import express,{Request,Response} from "express"
+import express, { Request, Response } from "express"
 import dotenv from "dotenv"
 import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
-const prisma= new PrismaClient();
-const app=express();
+const prisma = new PrismaClient();
+const app = express();
 app.use(express.json());
 
-app.get('/finduser/:id',async(req:Request,res:Response)=>{
-    const userid=parseInt(req.params.id);
-    const response=await prisma.user.findUnique({
-        where:{id:userid},
+app.get('/finduser/:id', async (req: Request, res: Response) => {
+    const userid = parseInt(req.params.id);
+    const response = await prisma.user.findUnique({
+        where: { id: userid },
+        select:{
+            todos:true
+        }
     })
+    res.json({
+        msg: response
+    })
+
+})
+app.get('/gettodos',async(req:Request,res:Response)=>{
+    const response=await prisma.todo.findMany({});
     res.json({
         msg:response
     })
-    
 })
-app.post('/createuser',async(req:Request,res:Response)=>{
-    const {username,email,firstname,lastname,password}=req.body;
-    const newuser=await prisma.user.create({
-        data:{
+app.post('/createuser', async (req: Request, res: Response) => {
+    const { username, email, firstname, lastname, password } = req.body;
+    const newuser = await prisma.user.create({
+        data: {
             username,
             email,
             firstname,
@@ -30,9 +39,9 @@ app.post('/createuser',async(req:Request,res:Response)=>{
         }
     })
     res.json(newuser);
-    console.log("newuser = ",newuser);
+    console.log("newuser = ", newuser);
 })
-app.post('/createtodo',async(req:Request,res:Response)=>{
+app.post('/createtodo', async (req: Request, res: Response) => {
     const { title, description, iscompleted, userid } = req.body;
     const newtodo = await prisma.todo.create({
         data: {
@@ -46,6 +55,6 @@ app.post('/createtodo',async(req:Request,res:Response)=>{
 })
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("hmlo hmlo");
 })
