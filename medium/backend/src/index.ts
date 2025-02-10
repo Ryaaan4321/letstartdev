7d53e5e46a53cab8244e6347d88ea667bc8a23a1
Ryaaan4321 <aryan056400@gmail.com>
2025-02-10 17:35:33 +0530

    wrote the schema of the application

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -24,4 +24,4 @@ app.get('/api/delte/:id',(c)=>{
 export default app
 
 // psql 'postgres://avnadmin:AVNS_OTrZxEickAEy5lnFYEa@pg-31b63a0b-tempooooo.k.aivencloud.com:17949/defaultdb?sslmode=require'
-// DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiYjE3NzQwZDMtN2UwNS00ZTYyLTg0NDYtMjA3Nzc1MDhiYzg5IiwidGVuYW50X2lkIjoiMTNkOGZlYzFmNGQ2ZDY5YjNmNWI5MDM0Mzg2YWVmYzNkMWM1MWI3NTI1MDY4YzE4MzA4MWM2MDkxNTBmYWU0MCIsImludGVybmFsX3NlY3JldCI6ImI5OGFhYTE5LTNkNjYtNGQxZC04NmI3LWUwMmJlZTYxYjQwNiJ9.tZ5aO0Kj-tfi5oT2DpZepIBaDNktCmc4ZpUgOskbfZ0"
+
