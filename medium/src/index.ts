1bb716ce6a79c9dc01912bdd83d23b1570b7dfc3
Ryaaan4321 <aryan056400@gmail.com>
2025-02-14 17:51:55 +0530

    signup route is working

--- medium/src/index.ts
+++ medium/src/index.ts
@@ -1,14 +1,40 @@
-import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
+app.post('/signup', async (c) => {
+  console.log("Signup route hit!");
+  try {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+      datasources: {
+        db: { url: c.env.DATABASE_URL }
+      }
+    }).$extends(withAccelerate());
+    const response = await prisma.user.create({
+      data: {
+        name: body.name,
+        username: body.username,
+        email: body.email,
+        password: body.password,
+      }
+    })
+    return c.json({ message: "Signup successful" }, 201);
+  } catch (error) {
+    console.log(error );
+    return c.status(411);
+  }
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+});
+app.get('/',async(c)=>{
+  return c.text("hmlo hmlo bitch")
 })
+
 
 export default app
 
-
-// psql 'postgres://avnadmin:AVNS_OTrZxEickAEy5lnFYEa@pg-31b63a0b-tempooooo.k.aivencloud.com:17949/defaultdb?sslmode=require'
-
-// DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiMjUyZTkwMDMtOWYyNi00OTZmLWJiZDktYjEwZmRkN2M3ZGUwIiwidGVuYW50X2lkIjoiMGRlMDIxNDc1MGJmMTc2NGI3OTZmYjY0ZTliYzY3ODk4ZmRjNzU3YWViYTc5MGUzNzY2ODJiNDQ2NDE4ZTJjMyIsImludGVybmFsX3NlY3JldCI6IjQwMzdlNzUyLTYyMWItNDI2MS1hZDhhLWZiNTEzZGIzOGE3NyJ9.LTpWNEqWjLeIYkrTAbKaouaFlC86TrYLGuWn0eLBR2o"
