b5b3478ba932ba172a1978a39290c23aa0138229
Ryaaan4321 <aryan056400@gmail.com>
2025-02-15 19:09:53 +0530

    the middleware is working

--- medium/src/router/blog.router.ts
+++ medium/src/router/blog.router.ts
@@ -1,19 +1,38 @@
 import { Hono } from "hono";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
-import { primitiveMappings } from "openai/_vendor/zod-to-json-schema/index.mjs";
+import { use } from "hono/jsx";
 
 export const blogrouter = new Hono<{
     Bindings: {
         DATABASE_URL: string;
         JWT_SECRET: string
+    },Variables:{
+       userId:string
     };
 }>();
+
+blogrouter.use('/*',async(c,next)=>{
+    const authheader=c.req.header("Authorization")||"";
+    const user=await verify(authheader,c.env.JWT_SECRET);
+    console.log(typeof user);
+    if(user){
+        c.set("userId",user.id);
+        await next();
+        return;
+    }else{
+        return c.json({
+            msg:"there is an error on this auth middleware"
+        })
+    }
+})
 
 blogrouter.post('/create', async (c) => {
     try {
         const body = await c.req.json();
+        const userid=c.get("userId")
+        console.log("user from the post req = " ,userid);
         const prisma = new PrismaClient({
             datasources: {
                 db: { url: c.env.DATABASE_URL }
@@ -25,14 +44,14 @@ blogrouter.post('/create', async (c) => {
                 content: body.content,
                 thumbnail: body.thumbnail,
                 published: body.published,
-                authorid: body.authorid
+                authorid: parseInt(userid)
             }
         })
         return c.json({
             msg:newblog.id
-        })
+        },200)
     } catch (error) {
-
+      console.log(error);
     }
 })
 
