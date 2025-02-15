0971e56346ab8e4949ce962e203c4d4b346cfb62
Ryaaan4321 <aryan056400@gmail.com>
2025-02-15 14:00:35 +0530

    blog rotuer get req. is working

--- medium/src/router/blog.router.ts
+++ medium/src/router/blog.router.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { sign } from "hono/jwt";
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { primitiveMappings } from "openai/_vendor/zod-to-json-schema/index.mjs";
 
 export const blogrouter = new Hono<{
     Bindings: {
@@ -34,3 +35,23 @@ blogrouter.post('/create', async (c) => {
 
     }
 })
+
+// try to do the pagination in this blog 
+// so that u can show only the some number of blogs 
+// at a page and the rest of the blogs will be shown 
+// in the next page
+blogrouter.get('/bulk',async(c)=>{
+    try{
+        const prisma = new PrismaClient({
+            datasources:{
+                db:{url:c.env.DATABASE_URL}
+            }
+        }).$extends(withAccelerate());
+        const bulkblogs=await prisma.blog.findMany();
+        return c.json(bulkblogs);
+    }catch(error){
+        return c.json({
+            error
+        })
+    }
+});
