6db5dea1398d86be307bd57eebbc31a048d4c1c9
Ryaaan4321 <aryan056400@gmail.com>
2025-02-24 17:59:44 +0530

    hope so i will complete this today

--- medium/frontend/src/components/FullBlog.tsx
+++ medium/frontend/src/components/FullBlog.tsx
@@ -13,12 +13,18 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
     return (
         <div>
             <AppBar />
-            <div className="grid grid-cols-12 px-10 w-full mt-8">
-                <div className="col-span-8 bg-red-700">
-                    <div className="text-3xl font-extrabold">{blog.title}</div>
-                    <div className="text-xl font-sm">{blog.content}</div>
+            <div className="flex justify-center">
+                <div className="grid grid-cols-12 px-10 w-full pt-12 max-w-screen-xl">
+                    <div className="col-span-8 ">
+                        <div className="text-3xl font-extrabold">{blog.title}</div>
+                        <div className="text-xl font-sm pt-4">{blog.content}</div>
+                        <div className="text-slate-400 font-sm pt-4">Posted on 2nd Dec 2024</div>
+                    </div>
+                    <div className="col-span-4 bg-green-200 ">Author
+                        <div className="font-bold text-xl">{blog.author.name || "Anonymous"}</div>
+                        <div>Random ability to catc the user attention</div>
+                    </div>
                 </div>
-                <div className="col-span-4">second one</div>
             </div>
         </div>
     );
